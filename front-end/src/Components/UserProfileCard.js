6920ca84d4efd811cd72c64ca7a868b776d15d32
Your Name <you@example.com>
2019-07-29 18:37:44 -0400

    Changed ProfileGrid and ProfileCard component into UserProfileCard, created Form

---
+++ front-end/src/Components/UserProfileCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const UserProfileCard = (props) => {
+    console.log('props', props)
+
+    return (
+        <div>
+            <div></div>
+        </div>
+    )
+}
+
+export default UserProfileCard;
