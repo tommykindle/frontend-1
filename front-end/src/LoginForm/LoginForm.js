809b0f9b699f07ddea982135b1f94ccd79e18d0b
Tommy <tommykindle83@gmail.com>
2019-07-30 13:10:40 -0700

    Fixed final version of styling

--- front-end/src/LoginForm/LoginForm.js
+++ front-end/src/LoginForm/LoginForm.js
@@ -39,37 +39,40 @@ const LoginForm = () => (
       return (
         <div className='formHolder'>
           <form onSubmit={handleSubmit}>
-            <h1>Friend Finder</h1>
-            <label htmlFor="email">Email</label>
-            <input
-              name="email"
-              type="text"
-              placeholder="Enter your email"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={errors.email && touched.email && "error"}
-            />
-            {errors.email && touched.email && (
-              <div className="input-feedback">{errors.email}</div>
-            )}
-            <label htmlFor="email">Password</label>
-            <input
-              name="password"
-              type="password"
-              placeholder="Enter your password"
-              value={values.password}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={errors.password && touched.password && "error"}
-            />
-            {errors.password && touched.password && (
-              <div className="input-feedback">{errors.password}</div>
-            )}
-            <button type="submit" disabled={isSubmitting}>
-              Login
+            <div className='Margin'>
+              <h1>Friend Finder</h1>
+              <label htmlFor="email">Email</label>
+              <input
+                name="email"
+                type="text"
+                placeholder="Enter your email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.email && touched.email && "error"}
+              />
+              {errors.email && touched.email && (
+                <div className="input-feedback">{errors.email}</div>
+              )}
+              <label htmlFor="email">Password</label>
+              <input
+                name="password"
+                type="password"
+                placeholder="Enter your password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.password && touched.password && "error"}
+              />
+              {errors.password && touched.password && (
+                <div className="input-feedback">{errors.password}</div>
+              )}
+              <button type="submit" disabled={isSubmitting}>
+                Login
           </button>
-            <button type="submit" formaction="/">SignUp</button>
+              <button type="submit" formAction="/">
+                SignUp</button>
+            </div>
           </form>
         </div>
       );
